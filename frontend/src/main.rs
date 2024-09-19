6ff803c87319933b900104b7f5f75e9dd8c626ec
elton-cs <govada.elton@gmail.com>
2024-09-19 15:44:58 -0400

    Fix duplicated DefaultPlugins

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,5 +3,14 @@ use bevy::prelude::*;
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins(DefaultPlugins).run();
+    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
+    app.add_systems(Startup, default_camera);
+
+    app.run();
+}
+
+fn default_camera(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scale = 0.3;
+    commands.spawn(camera_bundle);
 }
