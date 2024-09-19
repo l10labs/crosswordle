1076ea04ecdf46d5dff5a237c992bd0734bf0d9d
elton-cs <govada.elton@gmail.com>
2024-09-19 13:45:55 -0500

    Add bevy and empty window

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,13 +1,17 @@
+use bevy::prelude::*;
 use web_sys::window;
 
 fn main() {
-    console_error_panic_hook::set_once();
+    // console_error_panic_hook::set_once();
+    // let document = window()
+    //     .and_then(|win| win.document())
+    //     .expect("Could not access the document");
+    // let body = document.body().expect("Could not access document.body");
+    // let text_node = document.create_text_node("Welcome To CrossWordle");
+    // body.append_child(text_node.as_ref())
+    //     .expect("Failed to append text");
 
-    let document = window()
-        .and_then(|win| win.document())
-        .expect("Could not access the document");
-    let body = document.body().expect("Could not access document.body");
-    let text_node = document.create_text_node("Welcome To CrossWordle");
-    body.append_child(text_node.as_ref())
-        .expect("Failed to append text");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins).run();
 }
