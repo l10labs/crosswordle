d110350b6119667ed96475a4a223c33589b84aad
elton-cs <govada.elton@gmail.com>
2024-09-20 01:42:35 -0400

    Fix image rendering

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,12 +1,37 @@
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 
 fn main() {
-    let mut app = App::new();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "CrossWordle".to_string(), // ToDo
+                        // Bind to canvas included in `index.html`
+                        canvas: Some("#bevy".to_owned()),
+                        fit_canvas_to_parent: true,
+                        // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Startup, default_camera)
+        .run();
+}
 
-    app.add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()));
-    app.add_systems(Startup, default_camera);
-
-    app.run();
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("keys.png"),
+        ..default()
+    });
 }
 
 fn default_camera(mut commands: Commands) {
